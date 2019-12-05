9d797664cb676d005efacf74f8c45d18617f1711
Neil Sainsbury <neil.sainsbury@gmail.com>
2019-12-05 10:34:13 +1000

    Completed Day 3 (Part 2)

--- day03b.py
+++ day03b.py
@@ -1,21 +1,28 @@
+from sys import maxsize
+
 def main():
     with open('day03_input.txt') as file:
         wire1_steps = file.readline().strip().split(',')
         wire2_steps = file.readline().strip().split(',')
 
-    wire1_visited = set()
-    process_steps(wire1_steps, wire1_visited)
+    wire1_visited = process_steps(wire1_steps)
+    wire2_visited = process_steps(wire2_steps)
 
-    wire2_visited = set()
-    process_steps(wire2_steps, wire2_visited)
+    intersections = set(wire1_visited) & set(wire2_visited)
 
-    intersections = wire1_visited & wire2_visited
-    closest = min(intersections, key=lambda t: abs(t[0]) + abs(t[1]))
-    print(f"The closest point is {closest} with manhattan distance {abs(closest[0]) + abs(closest[1])}")
+    min_steps = maxsize
+    for intersection in intersections:
+        steps = wire1_visited.index(intersection) + wire2_visited.index(intersection) + 2
+        if steps < min_steps:
+            min_steps = steps
+
+    print(min_steps)
 
 
-def process_steps(steps, visited):
+def process_steps(steps):
     curr = (0, 0)
+    visited = []
+
     for step in steps:
         direction = step[0]
         n = int(step[1:])
@@ -30,7 +37,9 @@ def process_steps(steps, visited):
             new = [(curr[0] - i, curr[1]) for i in range(1, n + 1)]
 
         curr = new[-1]
-        visited.update(new)
+        visited += new
+
+    return visited
 
 
 if __name__ == '__main__':
